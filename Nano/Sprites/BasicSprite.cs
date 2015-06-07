ca0405c743679588b1badc633b279c3f99de23b5
SirNanoCat <SirNanoCat@gmail.com>
2015-06-07 07:15:15 +0100

    Added simple sprite stubs
    Added stubs for general ISprite interface and stub for BasicSprite class

---
+++ Nano/Sprites/BasicSprite.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Nano.Sprites
+{
+    public class BasicSprite : ISprite
+    {
+        #region ISprite implementation
+
+        public void Update(GameTime gameTime)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Vector2 Position
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public int Width
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public int Height
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        #endregion
+
+        public BasicSprite()
+        {
+        }
+    }
+}
+
---
+++ Nano/Sprites/ISprite.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Nano.Sprites
+{
+	public interface ISprite
+	{
+		Vector2 Position { get; set; }
+
+		int Width { get; set; }
+
+		int Height { get; set; }
+            
+        void Update(GameTime gameTime);
+
+        void Draw(GameTime gameTime, SpriteBatch spriteBatch);
+    
+    }
+}
+
