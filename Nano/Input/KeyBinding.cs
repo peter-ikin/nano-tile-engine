ceb3685d14b75f98ef9b8c079aa94803a101c5e1
SirNanoCat <SirNanoCat@gmail.com>
2015-06-07 09:11:56 +0100

    added stub KeyBinding class

---
+++ Nano/Input/KeyBinding.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace Nano.Input
+{
+	/// <summary>
+	/// The game adds a bunch of commands to the input manager and a KeyBinding
+	/// is created that maps the commands to the key press. 
+	/// The KeyBindings themselves are serialized out to a file.
+	/// </summary>
+	public class KeyBinding
+	{
+		public KeyBinding ()
+		{
+		}
+	}
+}
+
