b3504fe9b5bbadb33dee1022d20eb6a3f6eec3b2
SirNanoCat <SirNanoCat@gmail.com>
2015-06-15 07:04:38 +0100

    Added simple logging class
    Added simple file logger and service interface

---
+++ Nano/Engine/Sys/ILoggingService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Nano.Engine.Sys
+{
+    public enum LogLevel
+    {
+        Info,
+        Warning,
+        Error
+    }
+
+    public interface ILoggingService
+    {
+        void Log(string message, LogLevel logLevel);
+    }
+}
+
---
+++ Nano/Engine/Sys/SimpleFileLogger.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using OpenTK.Graphics.ES10;
+
+namespace Nano.Engine.Sys
+{
+    public class SimpleFileLogger : ILoggingService , IDisposable
+    {
+        private StreamWriter m_Stream;
+
+        public SimpleFileLogger(string filePath,bool append)
+        {
+            m_Stream = new StreamWriter(filePath, append);
+            m_Stream.AutoFlush = true;
+        }
+
+        #region IDisposable implementation
+
+        public void Log(string message, LogLevel logLevel)
+        {
+            string outputString = string.Format("{0} : {1} : {2}", 
+                DateTime.Now,
+                logLevel.ToString(), 
+                message 
+                );
+            m_Stream.WriteLine(outputString);
+        }
+
+        public void Dispose()
+        {
+            if(m_Stream != null)
+            {
+                m_Stream.Close();
+                m_Stream = null;
+            }
+        }
+
+        #endregion
+    }
+}
+
