9ae77915ca27cb164d19656a06fcb9066422f00a
SirNanoCat <SirNanoCat@gmail.com>
2015-06-16 23:12:02 +0100

    Added NullLogger class

---
+++ Nano/Engine/Sys/NullLogger.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Nano.Engine.Sys
+{
+    public class NullLogger : ILoggingService
+    {
+        public NullLogger()
+        {
+        }
+
+        #region ILoggingService implementation
+
+        public void Log(string message, LogLevel logLevel = LogLevel.Info)
+        {
+            //Intentionally drop all log messages
+        }
+
+        #endregion
+    }
+}
+
