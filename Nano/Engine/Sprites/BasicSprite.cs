b8714c0d48367596c7346747ca512f0f470467f4
SirNanoCat <SirNanoCat@gmail.com>
2015-06-18 08:27:27 +0100

    Added comments to the BasicSprite class

--- Nano/Engine/Sprites/BasicSprite.cs
+++ Nano/Engine/Sprites/BasicSprite.cs
@@ -4,6 +4,10 @@ using Microsoft.Xna.Framework.Graphics;
 
 namespace Nano.Engine.Sprites
 {
+    /// <summary>
+    /// Basic sprite.
+    /// Basic non-animated sprite implementation, supports translation and rotation
+    /// </summary>
     public class BasicSprite : ISprite
     {
         private readonly Texture2D m_Texture;
@@ -14,39 +18,69 @@ namespace Nano.Engine.Sprites
 
         #region ISprite implementation
 
+        /// <summary>
+        /// Update the sprite to the supplied GameTime
+        /// </summary>
+        /// <param name="gameTime">Game time.</param>
         public void Update(GameTime gameTime)
         {
             m_Rotation = MathHelper.WrapAngle(m_Rotation);
         }
 
+        /// <summary>
+        /// Draw the sprite
+        /// </summary>
+        /// <param name="spriteBatch">Sprite batch to draw with</param>
         public void Draw(SpriteBatch spriteBatch)
         {
             spriteBatch.Draw(m_Texture, m_Position, m_SourceRectangle, 
                 Color.White, m_Rotation, m_Origin, 1.0f, SpriteEffects.None, 1);
         }
 
+        /// <summary>
+        /// Gets or sets the position of the sprite ( in screen co-ordinates )
+        /// </summary>
+        /// <value>The position.</value>
         public Vector2 Position
         {
             get { return m_Position; }
             set { m_Position = value; }
         }
 
+        /// <summary>
+        /// Gets the width of the sprite (in pixels)
+        /// </summary>
+        /// <value>The width.</value>
         public int Width
         {
             get { return m_SourceRectangle.Width; }
         }
 
+        /// <summary>
+        /// Gets the height of the sprite (in pixels)
+        /// </summary>
+        /// <value>The height.</value>
         public int Height
         {
             get { return m_SourceRectangle.Height; }
         }
 
+        /// <summary>
+        /// Gets or sets the rotation angle of the sprite (in radians)
+        /// </summary>
+        /// <value>The rotation.</value>
         public float Rotation
         {
             get { return m_Rotation; }
             set { m_Rotation = value; }
         }
 
+        /// <summary>
+        /// Gets or sets the rotation origin.
+        /// This is the point (on the sprite) around which any rotation is performed.
+        /// By default the rotation origin is set to be the center of the sprite
+        /// </summary>
+        /// <value>The rotation origin.</value>
         public Vector2 RotationOrigin 
         { 
             get{ return m_Origin; } 
@@ -54,6 +88,10 @@ namespace Nano.Engine.Sprites
         }
         #endregion
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Nano.Engine.Sprites.BasicSprite"/> class.
+        /// </summary>
+        /// <param name="texture">The Texture that the sprite is based on</param>
         public BasicSprite(Texture2D texture)
         {
             m_Texture = texture;
