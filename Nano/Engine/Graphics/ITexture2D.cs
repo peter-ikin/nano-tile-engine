dc336edb89aceb489200d742e4059908ebeaaa67
SirNanoCat <SirNanoCat@gmail.com>
2015-06-20 07:03:58 +0100

    Added interface to represent 2D textures
    Added an interface ITexture2D that will be used to remove direct
    dependencies on concrete Texture2D xna class.

---
+++ Nano/Engine/Graphics/ITexture2D.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Nano.Engine.Graphics
+{
+    /// <summary>
+    /// Interface to abstract 2D textures
+    /// </summary>
+    public interface ITexture2D
+    {
+        /// <summary>
+        /// Gets the width of the texture in pixels.
+        /// </summary>
+        /// <value>The width in pixels.</value>
+        int Width { get; }
+
+        /// <summary>
+        /// Gets the height of the texture in pixels.
+        /// </summary>
+        /// <value>The height in pixels.</value>
+        int Height { get; }
+    }
+}
+
